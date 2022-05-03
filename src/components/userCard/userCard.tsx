4403a1b9d1b0776991342bc2851d58a87bda7ec3
Luncode <1370655052@qq.com>
2022-05-03 23:44:06 +0800

    update project

---
+++ src/components/userCard/userCard.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+const UserCard:FC = () => {
+    return(
+        <div>
+            用户卡片
+        </div>
+    )
+}
+
+export default UserCard
---
+++ src/components/videoCard/videoCard.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+
+const VideoCard:FC = () => {
+    return(
+        <div>
+            视频卡片
+        </div>
+    )
+}
+
+export default VideoCard
