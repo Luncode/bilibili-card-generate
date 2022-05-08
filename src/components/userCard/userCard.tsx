f4885036ce9ba4f604fb1a1a012cb83621eb6bba
Luncode <1370655052@qq.com>
2022-05-08 21:32:21 +0800

    userCard组件封装

--- src/components/userCard/userCard.tsx
+++ src/components/userCard/userCard.tsx
@@ -1,11 +1,24 @@
-import { FC } from "react"
+import { FC } from "react";
+import { IBILIBILI_USER_INFO } from "../../IType/interface";
 
-const UserCard:FC = () => {
-    return(
-        <div>
-            用户卡片
-        </div>
-    )
+interface IUserCard{
+    userInfo:IBILIBILI_USER_INFO
 }
 
-export default UserCard
+const UserCard: FC<IUserCard>= ({userInfo}) => {
+  return (
+    <div className="userInfo">
+      <div className="">{userInfo?.data.name}</div>
+      <img className="userFace" src={userInfo?.data.face} alt="" />
+      <div className="userStatus">
+        <div>
+          直播间：{userInfo.data.live_room.liveStatus == 1 ? "在线" : "离线"}
+        </div>
+        <div>用户等级：{userInfo.data.level}</div>
+        <div>硬币数量：{userInfo.data.coins}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
