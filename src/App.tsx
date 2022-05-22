ffff34da103acbf1c224d34d2755ca402406c3f2
Luncode <1370655052@qq.com>
2022-05-22 20:04:26 +0800

    添加html2canvas插件 生成base64图片

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import html2canvas from "html2canvas";
 import { useEffect, useState } from "react";
 import "./App.css";
 import UserCard from "./components/userCard";
@@ -5,12 +6,22 @@ import { IBILIBILI_USER_INFO } from "./IType/interface";
 
 function App() {
   const [userInfo, setUserInfo] = useState<IBILIBILI_USER_INFO>();
-  const [MidNumber, setMidNumber] = useState<string>('');
+  const [MidNumber, setMidNumber] = useState<string>("");
+  const [loadImages,setLoadImages] = useState("");
   const getUserInfo = () => {
-    if(!MidNumber) return
+    if (!MidNumber) return;
     fetch("/bilibili/x/space/acc/info?mid=" + MidNumber).then(async (res) => {
       setUserInfo((await res.json()) as IBILIBILI_USER_INFO);
       // console.log(userInfo?.data);
+    });
+  };
+  const loadImage = () => {
+    new html2canvas(document.getElementById("card"), {
+      useCORS: true,
+      allowTaint: true,
+    }).then((canvas: any) => {
+      setLoadImages(canvas.toDataURL())
+      console.log(canvas.toDataURL());
     });
   };
   // useEffect(()=>{
@@ -34,10 +45,22 @@ function App() {
             }}
           />
           <button onClick={getUserInfo}>获取</button>
+          <button onClick={loadImage}>生成</button>
         </div>
-        {userInfo && (
-         <UserCard userInfo={userInfo}/>
-        )}
+        <div id="card">{userInfo && <UserCard userInfo={userInfo} />}</div>
+        <div>
+          <h4>复制base64代码</h4>
+          <textarea value={loadImages}/>
+        </div>
+        {
+          loadImages && (
+            <>
+              图片预览效果
+              <img src={loadImages} alt="" />
+            </>
+          )
+        }
+       
       </header>
     </div>
   );
